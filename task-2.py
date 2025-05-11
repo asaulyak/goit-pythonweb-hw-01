b15a75b793346ee02703a9864d83e750ae4ab153
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-05-11 20:22:45 +0300

    add task 2

---
+++ task-2.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    level=logging.INFO,
+    handlers=[logging.StreamHandler()],
+)
+
+
+class Book:
+    def __init__(self, title: str, author: str, year: str):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title}, {self.author}, {self.year}"
+
+
+class LibraryInterface(ABC):
+    @abstractmethod
+    def add_book(self, book: Book):
+        pass
+
+    @abstractmethod
+    def remove_book(self, title: str):
+        pass
+
+    @abstractmethod
+    def list_books(self):
+        pass
+
+
+class Library(LibraryInterface):
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+
+    def remove_book(self, title: str):
+        book_to_remove = next(book for book in self.books if book.title == title)
+
+        if book_to_remove:
+            self.books.remove(book_to_remove)
+
+    def list_books(self):
+        return self.books
+
+
+class LibraryManager:
+    def __init__(self, library: LibraryInterface):
+        self.library = library
+
+    def add_book(self, title: str, author: str, year: str):
+        self.library.add_book(Book(title, author, year))
+
+    def remove_book(self, title: str):
+        self.library.remove_book(title)
+
+    def show_books(self):
+        for book in self.library.list_books():
+            logging.info(book)
+
+
+def main():
+    library: LibraryInterface = Library()
+    manager: LibraryManager = LibraryManager(library)
+
+    while True:
+        command = input("Enter command (add, remove, show, exit): ").strip().lower()
+
+        match command:
+            case "add":
+                title = input("Enter book title: ").strip()
+                author = input("Enter book author: ").strip()
+                year = input("Enter book year: ").strip()
+                manager.add_book(title, author, year)
+            case "remove":
+                title = input("Enter book title to remove: ").strip()
+                manager.remove_book(title)
+            case "show":
+                manager.show_books()
+            case "exit":
+                break
+            case _:
+                print("Invalid command. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
